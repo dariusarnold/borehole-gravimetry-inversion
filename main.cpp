ae5be52a6554e2cd024897fc3007184e97856b13
Darius Arnold <mail@dariusarnold.de>
2018-10-30 21:16:41 +0100

    Add documentation to functions that had none

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 /**
  * Struct that holds measurement data read from file and associates the data
  * with a label which describes the data.
+ * depth unit is Meters, with a positive Z axis down the borehole.
+ * grav unit is mGal.
  */
 struct MeasurementData{
     double depth;
@@ -45,6 +47,12 @@ public:
 };
 
 
+/**
+ * Open .dat file and read measurements.
+ * It is expected that data is given in depth, gravity measurement order.
+ * @param filepath
+ * @return Vector holding MeasurementData objects, which represent one row from the file
+ */
 std::vector<MeasurementData> MeasurementReader::read_measurements_file(const std::string& filepath) {
     std::ifstream matrixFile(filepath);
     std::vector<MeasurementData> output;
@@ -58,6 +66,10 @@ std::vector<MeasurementData> MeasurementReader::read_measurements_file(const std
     return output;
 }
 
+
+/*
+ * print a vector of printable elements to ostream
+ */
 template <typename T>
 std::ostream& operator<<(std::ostream& os, std::vector<T> vec){
     for (auto el : vec){
