c71140cca3ce39e60aa859439b9ed809454255d0
Darius Arnold <mail@dariusarnold.de>
2018-12-06 21:27:06 +0100

    Add program to generate synthetic borehole gravity measurement data

--- source/model-maker/synthetic.py
+++ source/model-maker/synthetic.py
@@ -2,14 +2,31 @@
 Genereate snythetic density measurements
 """
 
-import matplotlib.pyplot as plt
-from DensityModel import make_model_arange
+import numpy as np
+
+from DensityModel import DensityModel
+from SyntheticGenerator import SyntheticGenerator
+
+
+def save_synthetic_data(fname, density_background, density_spike, spike_top, spike_width, measurement_depths):
+    """
+    Generate synthetic data for a density model with constant background density and one density spike.
+    Data is saved to file
+    :param fname: filename
+    :param density_background: background density in kg/m³
+    :param density_spike: density of spike in kg/m³
+    :param spike_top: depth of the top of the spike layer in m
+    :param spike_width: in m
+    :param measurement_depths: iterable of depths at which synthetic data should be created
+    :return: None
+    """
+    dm = DensityModel(density_background, spike_top, spike_width, density_spike)
+    SyntheticGenerator.save_to_file(fname, dm, measurement_depths)
 
 
 def main():
-    dm = make_model_arange(1000, 10, 5, 2000, 30, 1)
-    plt.plot(dm.depths, dm.densities, ".")
-    plt.show()
+    depths = np.array((25., 66., 143.))
+    save_synthetic_data("test.dat", 2700, 3500, 40, 5, depths)
 
 
 if __name__ == '__main__':
