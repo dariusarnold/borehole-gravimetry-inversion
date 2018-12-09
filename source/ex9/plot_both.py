ea09fcbd8c6cfb3a38dd424ee5592b8396d9b94d
Darius Arnold <mail@dariusarnold.de>
2018-12-09 18:30:23 +0100

    Add script that plots results of a synthetic inversion

---
+++ source/ex9/plot_both.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    # load data from density model
+    depth_dm, dens_dm = np.loadtxt("test_dm.dens", unpack=True)
+    # load resulting density model from inversion
+    depth_inv, dens_inv = np.loadtxt("test_inversion.dens", unpack=True)
+    # convert to kg/m3
+    dens_inv *= 1000
+
+    fig, ax1 = plt.subplots()
+    ax1.plot(depth_dm, dens_dm, color="red", label="Density model")
+    ax2 = ax1.twinx()
+    ax1.plot(depth_inv, dens_inv, color="blue", label="Inversion result")
+    ax1.legend()
+    ax1.set_ylabel("Density (kg/m³)")
+    ax1.set_xlabel("Depth (m)")
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
