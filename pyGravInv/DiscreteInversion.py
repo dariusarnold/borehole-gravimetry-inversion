5e1e71e9ab7224c0ab1c1f1138a872c74b2cebbb
Darius Arnold <mail@dariusarnold.de>
2019-01-02 19:49:22 +0100

    Add function that calculates optimal nu by bisection and function to calculate the model

--- pyGravInv/DiscreteInversion.py
+++ pyGravInv/DiscreteInversion.py
@@ -211,7 +211,63 @@ def ex11_2(fname):
     # calculate misfit for multiple values of nu
     range_of_nus = np.logspace(1E-6, 1E2, 10000)
     range_of_chis = np.array([misfit_squared(d_double_prime, nu, Lambda) for nu in range_of_nus])
-    plt.plot(range_of_nus, range_of_chis)
+    plt.plot(range_of_nus, range_of_chis, ".")
+    plt.show()
+
+
+def calculate_coeffs(new_data, nu, Lambda):
+    return new_data / ( (1/nu + 1/Lambda) + Lambda )
+
+
+def calculate_model(S_inverted, U, new_coefficients):
+    return np.dot(np.dot(S_inverted, U), new_coefficients)
+
+
+def optimal_nu_bysection(target_misfit, new_data, Lambda, accuracy=0.01):
+    """
+    Calculate the optimal Lagrange parameter using a bisection method.
+    The optimum nu maximizes the misfit.
+    :param target_misfit:
+    :return:
+    """
+    import sys
+    # set borders of interval in which to search
+    nu_left = sys.float_info.epsilon
+    nu_right = 1E6
+    chi_squared_left = misfit_squared(new_data, nu_left, Lambda)
+    chi_squared_right = misfit_squared(new_data, nu_right, Lambda)
+    # misfit over nu is a monotonically falling function, check if target_misfit can be reached
+    assert chi_squared_left > target_misfit > chi_squared_right, "Target misfit was set outside of possible range"
+
+    nu_mid = (nu_left + nu_right) / 2
+    chi_squared = misfit_squared(new_data, nu_mid, Lambda)
+    while abs(chi_squared - target_misfit) > accuracy:
+        nu_mid = (nu_left + nu_right) / 2
+        chi_squared = misfit_squared(new_data, nu_mid, Lambda)
+        if chi_squared > target_misfit:
+            nu_left = nu_mid
+        else:
+            nu_right = nu_mid
+    print(nu_mid)
+    return nu_mid
+
+
+def ex11_3(fname):
+    measurement_depths, measurement_dens, measurement_errors = np.loadtxt(fname, unpack=True)
+    inversion_depths = get_inversion_depth_steps(measurement_depths)
+    V, Lambda, U_transposed = do_SVD(fname)
+    desired_misfit = len(measurement_depths)
+    # calculate new data
+    d_double_prime = new_data(measurement_dens, np.transpose(V),  measurement_errors)
+    # calculate desired misfit using bisection
+    optimal_nu = optimal_nu_bysection(desired_misfit, d_double_prime, Lambda)
+    # calculate new coefficients
+    alpha_double_prime = calculate_coeffs(d_double_prime, optimal_nu, Lambda)
+    S_shape = (len(inversion_depths), len(inversion_depths))
+    dx = inversion_depths[1] - inversion_depths[0]
+    S_inverted = matrix_S_inverted(gamma=1E-3, shape=S_shape, delta_x=dx)
+    dens_model = calculate_model(S_inverted, np.transpose(U_transposed), new_coefficients=alpha_double_prime)
+    plt.plot(dens_model)
     plt.show()
 
 
@@ -219,6 +275,7 @@ def ex11_2(fname):
 def main():
     #ex11_1("data/grav15.dat")
     ex11_2("data/grav15.dat")
+    ex11_3("data/grav15.dat")
 
 
 if __name__ == '__main__':
