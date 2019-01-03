5749e22acc94fbab5f22bdc8e9b270901fe69d92
Darius Arnold <mail@dariusarnold.de>
2019-01-03 14:36:07 +0100

    Move call of selected function outside of try
    This is done because an AuttributeError in the called function would be caught by the except statement and treated as if the function wasn't found.

--- pyGravInv/DiscreteInversion.py
+++ pyGravInv/DiscreteInversion.py
@@ -186,6 +186,8 @@ def get_inversion_depth_steps(measurement_depths, num_steps=10000):
     :param num_steps: Number of steps used for discretozation
     :return:
     """
+    # add 2 to get the same result as the C++ code, where two additional steps are added so the effect for some norms
+    # that occurs when going past the last measurement point is seen
     num_steps += 2
     return np.linspace(0, measurement_depths[-1], num_steps)
 
@@ -280,9 +282,10 @@ def main():
     for s in selection:
         try:
             function_name = getattr(__import__(__name__), f"ex11_{s}")
-            function_name("data/grav15.dat")
         except AttributeError:
             print(f"Task {s} is not available")
+            return
+        function_name("data/grav15.dat")
 
 
 if __name__ == '__main__':
