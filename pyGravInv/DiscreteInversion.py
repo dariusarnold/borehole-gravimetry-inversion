b40c934d46e923dcc5e5c9f0e54696c601daa0c4
Darius Arnold <mail@dariusarnold.de>
2019-01-07 16:00:36 +0100

    Rewrite B calculation

--- pyGravInv/DiscreteInversion.py
+++ pyGravInv/DiscreteInversion.py
@@ -33,78 +33,39 @@ def matrix_Sigma_inverted(errors):
     return Sigma_inverted
 
 
+def calculate_B_row(depth_distribution, depth_j):
+    """
+    Calculate a row of the B matrix
+    :param depth_distribution: ndarray containing inversion depths
+    :param depth_j: measurement depth for which to calculate row
+    :return: row of B
+    """
+    gamma = 0.08382  # (mGal cm^3)/(m g)
+    z = depth_distribution
+    alpha = np.searchsorted(z, depth_j) - 1
+    del_j = (depth_j - z[alpha])/(z[alpha+1] - z[alpha])
+
+    B_j = np.zeros(z.size)
+    if alpha == 0:
+        B_j[alpha] = (2-del_j)*del_j*(z[alpha+1] - z[alpha])
+    else:
+        B_j[0] = (z[1]-z[0])
+        for i in range(1, alpha):
+            B_j[i] = (z[i]-z[i-1]) + (z[i+1]-z[i])
+        B_j[alpha] = (z[alpha]-z[alpha-1]) + (2 - del_j)*del_j*(z[alpha+1] - z[alpha])
+    B_j[alpha+1] = del_j**2*(z[alpha+1] - z[alpha])
+    B_j *= -0.5*gamma
+    return B_j
+
+
 def matrix_B(measurement_depths, inversion_depths):
     """
-    Create full matrix B for a gravimetry inversion
-    :param measurement_depths: numpy array containing the depths where values were measured
-    :param inversion_depths: numpy array containing the depths where the inversion result was discretized
-    :return:
+    Calculate B matrix from B rows.
+    :return: B matrix
     """
-
-    def find_closest_index(array, value):
-        """
-        Return the index of the elements that are closest to value in array
-        :param array:
-        :param value:
-        :return:
-        """
-        return np.argmin(np.abs(array-value))
-
-
-    def find_closest_le(array, value):
-        """
-        Return the index of the element in array that is closest to value, but <= value
-        :param array:
-        :param value:
-        :return:
-        """
-        return find_closest_index(array, math.floor(value))
-
-
-    def row_of_B(z_j, z_nu):
-        """
-        Calculate one row of the matrix B
-        :param z_j: Scalar value up to which
-        :type z_j: float
-        :param z_nu: vector with grid points
-        :type z_nu: np.ndarray
-        :return:
-        """
-
-        def delta_z(index):
-            """
-            Calculate delta z for a given index as a forward difference
-            :param index: index nu
-            """
-            return z_nu[index+1] - z_nu[index]
-
-        alpha = find_closest_le(z_nu, z_j)
-        phi = (z_j - z_nu[alpha]) / (z_nu[alpha+1] - z_nu[alpha])
-        gamma = 0.08382 # mGal*cm³/(m*g)
-        B = np.zeros(len(z_nu))
-        for nu in range(0, len(z_nu)):
-            # all indices in the conditions - 1 since in math the indices start at 1, but in python they start at 0
-            if nu == 0:
-                B[nu] = delta_z(nu)
-            elif 0 < nu < alpha-1:
-                B[nu] = delta_z(nu-1) + delta_z(nu)
-            elif nu == alpha-1:
-                B[nu] = delta_z(nu-1) + phi * delta_z(nu) * (2 - phi)
-            elif nu == alpha:
-                B[nu] = phi**2 * delta_z(nu-1)
-            else:
-                # B is default initialized as zero, so we dont have to set it here
-                continue
-        B *= -gamma/2
-        return B
-
-    # Create empty array. Since we want to append rows, we have to create it with the number of columns the rows are
-    # going to have
-    B = np.empty((0, len(inversion_depths)))
-    for z in measurement_depths:
-        B_row = row_of_B(z, inversion_depths)
-        # append row to matrix B
-        B = np.vstack((B, B_row))
+    B = np.zeros((measurement_depths.size, inversion_depths.size))
+    for i, zj in enumerate(measurement_depths):
+        B[i] = calculate_B_row(inversion_depths, zj)
     return B
 
 
